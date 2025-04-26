f2253639229ecded96962acf7e197d94b68f66f5
anywell <spmf5n69@voice.ocn.ne.jp>
2025-04-26 17:01:22 +0900

    comit

---
+++ test.c
@@ -0,0 +1,18 @@
+/*format91.c
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    a = 10; /*変数aに10を代入*/
+    b = 15; /*変数bに15を代入*/
+
+    c = a + b; /*変数cに(a+b)の値を代入*/
+
+    printf("%dたす%dは%dです\n", a, b, c);
+
+    return 0;
+}
